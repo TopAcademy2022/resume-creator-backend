89b920f458f726332a770f856eb23db64b8d16e4
TopAcademy2022 <popov_r_top@outlook.com>
2025-01-29 20:09:54 +0300

    Added project files. Closed #1

---
+++ src/main/java/resume/creator/backend/top/TopApplication.java
@@ -0,0 +1,11 @@
+package resume.creator.backend.top;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TopApplication.class, args);
+	}
+}
