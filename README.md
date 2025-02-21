188caa6f81836767358b75ec673af8f03891d267
TopAcademyBel <121047497+TopAcademy2022@users.noreply.github.com>
2025-02-21 20:32:54 +0300

    Added link core for build. Closed #19 (#20)

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # resume-creator-backend
 Java Spring Boot backend for resume creator.
+
+## How build project
+    1. Clone this repository;
+    2. Run script 'build-core.bat' for build logic jar.
+    3. Build project with Gradle or etc.;
---
+++ build-core.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+# Enable debugging
+set -x
+
+# Store the current directory
+currentDir=$(pwd)
+
+# Initialize and update git submodules
+git submodule init
+git submodule update
+
+# Change to the resume-creator-logic directory and build the project
+cd "lib/resume-creator-logic" || exit
+chmod +x ./gradlew
+./gradlew build
+
+# Change back to the original directory
+cd "$currentDir" || exit
+
+# Copy the built jar file to the lib directory
+cp lib/resume-creator-logic/app/build/libs/app.jar lib
