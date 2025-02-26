e761ea9b8ece6ac38d890eefad555c09b34b1c5c
TopAcademyBel <121047497+TopAcademy2022@users.noreply.github.com>
2025-02-26 20:17:34 +0300

    Added ResumeController. Closed #6 (#21)

---
+++ src/main/java/resume/creator/backend/top/controllers/ResumeController.java
@@ -0,0 +1,71 @@
+package resume.creator.backend.top.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import resume.creator.backend.top.models.Answer;
+import resume.creator.backend.top.models.ResumeQuestion;
+import resume.creator.backend.top.models.ResumeType;
+import resume.creator.logic.models.Resume;
+
+@RestController
+@RequestMapping("resume")
+public class ResumeController {
+    @GetMapping
+    public ResponseEntity<List<ResumeType>> GetResumeTypes() {
+        // Get List objects from service
+        // Test data
+        List<ResumeType> resumeTypes = new ArrayList<ResumeType>();
+
+        resumeTypes.add(new ResumeType("DefaultResume"));
+        resumeTypes.add(new ResumeType("TopResume"));
+
+        return ResponseEntity.ok(resumeTypes);
+    }
+
+    @GetMapping("list")
+    public ResponseEntity<List<Resume>> GetResumeTypes(int userId) {
+        // Get List objects from service
+        // Test data
+        List<Resume> createdResume = new ArrayList<Resume>();
+
+        Resume testResume = new Resume();
+        testResume.FIO = "Иванов Илья";
+        testResume.PathToFile = "./myfile.soprano";
+        testResume.PathToResumeTemplate = "./templates/mytemplate.docx";
+        createdResume.add(testResume);
+
+        return ResponseEntity.ok(createdResume);
+    }
+
+    @GetMapping("questions")
+    public ResponseEntity<List<ResumeQuestion>> GetResumeQuestions(String resumeName) {
+        // Get List objects from service
+        // Test data
+        List<ResumeQuestion> resumeTypes = new ArrayList<ResumeQuestion>();
+
+        resumeTypes.add(new ResumeQuestion("Сколько вам лет?", "int"));
+        resumeTypes.add(new ResumeQuestion("Какой ваш стаж?", "int"));
+
+        return ResponseEntity.ok(resumeTypes);
+    }
+
+    @PostMapping("answers")
+    public ResponseEntity<String> GetResumeQuestions(@RequestBody List<Answer> answers) {
+        // Get List objects from service
+        // Test data
+        if(answers.size() > 0){
+            return new ResponseEntity<String>("success", HttpStatus.CREATED);
+        }
+
+        return new ResponseEntity<String>("error", HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/resume/creator/backend/top/models/ResumeType.java
@@ -0,0 +1,9 @@
+package resume.creator.backend.top.models;
+
+public class ResumeType {
+    public String Name;
+
+    public ResumeType(String name){
+        this.Name = name;
+    }
+}
