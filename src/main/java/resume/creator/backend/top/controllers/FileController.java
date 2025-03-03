5b5b298484954ad06e5abfa255683c6a09ff6dd5
London3149-train <121044384+London3149-train@users.noreply.github.com>
2025-03-03 21:23:59 +0300

    Added UploadFile. Closed #24 (#26)

--- src/main/java/resume/creator/backend/top/controllers/FileController.java
+++ src/main/java/resume/creator/backend/top/controllers/FileController.java
@@ -1,6 +1,7 @@
 package resume.creator.backend.top.controllers;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
@@ -8,7 +9,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class FileController {
@@ -27,5 +30,24 @@ public class FileController {
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getName() + "\"")
                 .body(resource);
+    }
+
+    @PostMapping("/upload")
+    public boolean SevFileResumeTemplate(MultipartFile  file){
+        final String DirectoryPath = "files/";
+        try {
+            File dir = new File(DirectoryPath);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+
+            File serverFile = new File(dir.getAbsolutePath(), file.getOriginalFilename());
+            file.transferTo(serverFile);
+
+            return true;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
     }
 }
